f6267ce2179c3d1e612adbe9072bc6a7f5ef0610
Nghia Truong <nghiatruong.vn@gmail.com>
2018-10-27 18:59:38 -0600

    add all forward declarations for LibOpenGL

---
+++ LibOpenGL/Forward.h
@@ -0,0 +1,48 @@
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//    .--------------------------------------------------.
+//    |  This file is part of NTCodeBase                 |
+//    |  Created 2018 by NT (https://ttnghia.github.io)  |
+//    '--------------------------------------------------'
+//                            \o/
+//                             |
+//                            / |
+//
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#pragma once
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+namespace MeshObjects {
+class MeshObject;
+class CubeObject;
+class GridObject;
+class SphereObject;
+class WireFrameBoxObject;
+}
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+class Camera;
+
+class Lights;
+class DirectionalLights;
+class PointLights;
+class SpotLights;
+
+class Material;
+class OpenGLBuffer;
+class OpenGLTexture;
+
+class RenderObject;
+class SkyBoxRender;
+class PointLightRender;
+class WireFrameBoxRender;
+class OffScreenRender;
+class DepthBufferRender;
+class ScreenQuadTextureRender;
+class CheckerboardBackgroundRender;
+class GridBackgroundRender;
+class MeshRender;
+class PlaneRender;
+class ShaderProgram;
